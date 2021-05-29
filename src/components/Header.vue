fe0873310993683dac5b27dd46ba742284fb5ba1
khunemz <khunemz@gmail.com>
2021-05-29 12:27:48 +0700

    fetching data

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="mx-auto text-center bg-blue-800 text-white p-4 mb-10">
+    <div class="text-3xl md:text-4xl font-bold mb-3">
+      <i class="fa fa-viruses"></i>
+      COVID-19 Tracker
+    </div>
+    <p>API by 
+      <a href="https://covid19.api.com" class="text-blue-300" target="_blank">
+     covid19api.com</a>
+     </p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main v-if="!loading">
+    show data
+  </main>
+
+  <main v-else class="flext flex-col align-center justify-center text-center">
+    <div class="text-gray-500 text-3xl mt-10 mb-6">
+      Fetcing data
+    </div>
+    <img :src="loadingImage" class="w-24 m-auto" alt="">
+  </main>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      loading: true , 
+      title: 'GLOBAL',
+      dataDate: '',
+      stats: {},
+      contries: [],
+      loadingImage: require('../assets/hourglass.gif')
+    }
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch('https://api.covid19api.com/summary');
+      const data = await res.json();
+      return data;
+    }
+  } ,
+  async created() {
+    const data = await this.fetchCovidData();
+    this.dataDate = data.Date;
+    this.stats = data.Global;
+    this.country = data.Contries;
+    this.loading = false;
   }
 }
 </script>
